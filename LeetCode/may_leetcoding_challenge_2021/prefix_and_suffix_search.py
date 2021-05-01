0950e45917c5ab99010bd11c52445bfbdf53ab41
Md. Belal Hossain <bh.pro.pust@gmail.com>
2021-05-02 00:50:14 +0600

    again TLE

--- LeetCode/may_leetcoding_challenge_2021/prefix_and_suffix_search.py
+++ LeetCode/may_leetcoding_challenge_2021/prefix_and_suffix_search.py
@@ -4,26 +4,32 @@
 class WordFilter:
 
     def __init__(self, words):
-        self.words = words
+        self.words = words[::-1]
+        self.ws_len = len(words)
+        self.w_lens = [len(w) for w in words]
+        self.w_max_len = max(self.w_lens)
+        self.w_min_len = min(self.w_lens)
 
     def f(self, prefix, suffix):
-        for word, index in zip(self.words[::-1], range(len(self.words)-1, -1, -1)):
-            # print(prefix, word, suffix)
-            pre_len = len(prefix)
-            suf_len = len(suffix)
-            w_len = len(word)
-            if pre_len > w_len or suf_len > w_len:
+        pre_len = len(prefix)
+        suf_len = len(suffix)
+        is_identical = False
+        if pre_len > self.w_min_len or suf_len > self.w_min_len:
+            return -1
+        if prefix == suffix:
+            is_identical = True
+
+        for word, index, i in zip(self.words, range(self.ws_len-1, -1, -1), range(self.ws_len)):
+            if pre_len > self.w_lens[i] or suf_len > self.w_lens[i]:
                 continue
             else:
-                if prefix == word[0:pre_len] and suffix == word[w_len-suf_len:]:
+                if is_identical and self.w_lens[i] == pre_len:
+                    if word == prefix:
+                        return index
+                    continue
+                if prefix == word[0:pre_len] and suffix == word[self.w_lens[i]-suf_len:]:
                     return index
 
-
-            # if prefix == suffix and len(word) == len(prefix):
-            #     if word == prefix:
-            #         return index
-            #     else:
-            #         continue
             # pattern_pre = f'^{prefix}.*'
             # pattern_suf = f'.*{suffix}$'
             # if re.search(pattern_pre, word) and re.search(pattern_suf, word):
@@ -31,19 +37,23 @@ class WordFilter:
         return -1
 
 # words = ['apple', 'aphdjfheee']
-# prefix, suffix = 'ap', 'eee' 
+# prefix, suffix = 'ap', 'eee'
 # Your WordFilter object will be instantiated and called as such:
 # obj = WordFilter(words)
 # param_1 = obj.f(prefix,suffix)
 
 # print(param_1)
 
-# data = [[["cabaabaaaa","ccbcababac","bacaabccba","bcbbcbacaa","abcaccbcaa","accabaccaa","cabcbbbcca","ababccabcb","caccbbcbab","bccbacbcba"]],["bccbacbcba","a"],["ab","abcaccbcaa"],["a","aa"],["cabaaba","abaaaa"],["cacc","accbbcbab"],["ccbcab","bac"],["bac","cba"],["ac","accabaccaa"],["bcbb","aa"],["ccbca","cbcababac"]]
-# output = []
-# for i in range(1, len(data)):
-#     obj = WordFilter(data[0][0])
-#     index = obj.f(data[i][0],data[i][1])
-#     output.append(index)
-#     print(index)
 
-# print(output)
+""" TEST
+data = [[["cabaabaaaa", "ccbcababac", "bacaabccba", "bcbbcbacaa", "abcaccbcaa", "accabaccaa", "cabcbbbcca", "ababccabcb", "caccbbcbab", "bccbacbcba"]], ["bccbacbcba", "a"], [
+    "ab", "abcaccbcaa"], ["a", "aa"], ["cabaaba", "abaaaa"], ["cacc", "accbbcbab"], ["ccbcab", "bac"], ["bac", "cba"], ["ac", "accabaccaa"], ["bcbb", "aa"], ["ccbca", "cbcababac"]]
+output = []
+for i in range(1, len(data)):
+    obj = WordFilter(data[0][0])
+    index = obj.f(data[i][0], data[i][1])
+    output.append(index)
+    # print(index)
+
+print(output)
+"""
