1aca3c8e6cdefe2091ec5d5c0944caa3d3b2f97b
Md. Belal Hossain <bh.pro.pust@gmail.com>
2021-05-02 01:39:48 +0600

    Accepted!

--- LeetCode/may_leetcoding_challenge_2021/prefix_and_suffix_search.py
+++ LeetCode/may_leetcoding_challenge_2021/prefix_and_suffix_search.py
@@ -1,48 +1,17 @@
-###### TLE ########
-# import re
+###### Accepted  ########
 
 class WordFilter:
 
     def __init__(self, words):
-        self.words = words[::-1]
-        self.ws_len = len(words)
-        self.w_lens = [len(w) for w in words]
-        self.w_max_len = max(self.w_lens)
-        self.w_min_len = min(self.w_lens)
+        self.d = {}
+        for i, w in enumerate(words):
+            w_len = len(w)
+            for p in range(1, min(10, w_len)+1):
+                for s in range(1, min(10, w_len)+1):
+                    self.d[w[:p], w[-s:]] = i
 
     def f(self, prefix, suffix):
-        pre_len = len(prefix)
-        suf_len = len(suffix)
-        is_identical = False
-        if pre_len > self.w_min_len or suf_len > self.w_min_len:
-            return -1
-        if prefix == suffix:
-            is_identical = True
-
-        for word, index, i in zip(self.words, range(self.ws_len-1, -1, -1), range(self.ws_len)):
-            if pre_len > self.w_lens[i] or suf_len > self.w_lens[i]:
-                continue
-            else:
-                if is_identical and self.w_lens[i] == pre_len:
-                    if word == prefix:
-                        return index
-                    continue
-                if prefix == word[0:pre_len] and suffix == word[self.w_lens[i]-suf_len:]:
-                    return index
-
-            # pattern_pre = f'^{prefix}.*'
-            # pattern_suf = f'.*{suffix}$'
-            # if re.search(pattern_pre, word) and re.search(pattern_suf, word):
-            #     return index
-        return -1
-
-# words = ['apple', 'aphdjfheee']
-# prefix, suffix = 'ap', 'eee'
-# Your WordFilter object will be instantiated and called as such:
-# obj = WordFilter(words)
-# param_1 = obj.f(prefix,suffix)
-
-# print(param_1)
+        return self.d[prefix, suffix] if (prefix, suffix) in self.d else -1
 
 
 """ TEST
